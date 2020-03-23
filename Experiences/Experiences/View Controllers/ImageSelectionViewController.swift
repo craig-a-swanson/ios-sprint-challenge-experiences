cb75465f0727b3b34a847be84dd3709b671f4b01
Craig Swanson <cswanson02@me.com>
2020-03-22 23:06:52 -0500

    Update ImageSelectionViewController.swift
    Completed segue to audio recorder.

--- Experiences/Experiences/View Controllers/ImageSelectionViewController.swift
+++ Experiences/Experiences/View Controllers/ImageSelectionViewController.swift
@@ -129,16 +129,15 @@ class ImageSelectionViewController: ShiftableViewController {
                 view.layoutSubviews()
             }
     
-    
-    /*
      // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
+        if segue.identifier == "RecordAudioSegue" {
+            guard let audioRecordingVC = segue.destination as? AudioRecorderViewController else { return }
+            
+            audioRecordingVC.experienceTitle = titleTextField.text
+            audioRecordingVC.picture = imageView.image
+        }
      }
-     */
     
 }
 
